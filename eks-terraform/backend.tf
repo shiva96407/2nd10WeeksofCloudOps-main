e3ab8a7d33879188a9ecd5527b81c92a34b1505f
shiva96407 <arukalashivavision96@gmail.com>
2025-04-22 17:46:01 +0530

    Update backend.tf

--- eks-terraform/backend.tf
+++ eks-terraform/backend.tf
@@ -7,9 +7,9 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "werty098"
-    key    = "eks/terraform.tfstate"
-    region = "ap-south-1"
+    bucket = "east-12345678"
+    key    = "eggks/terraform.tfstate"
+    region = "us-east-1"
   }
 
   required_version = ">= 1.6.3"
