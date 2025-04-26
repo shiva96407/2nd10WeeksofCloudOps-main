77312bb0aa5d39b4e10b4f170446128c868afdbe
shiva96407 <arukalashivavision96@gmail.com>
2025-04-26 17:14:58 +0530

    Update config.js

--- client/src/pages/config.js
+++ client/src/pages/config.js
@@ -1,8 +1,4 @@
-// // const API_BASE_URL = "http://localhost:8800";
-const API_BASE_URL = "http://arukalashiva.shop";
-// export default API_BASE_URL;
-// const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || "http://backend";
-// export default API_BASE_URL;
-// const API_BASE_URL = "REACT_APP_API_BASE_URL_PLACEHOLDER";
-export default API_BASE_URL;
+
+const API_BASE_URL = "http://api.arukalashiva.shop";
+
 
